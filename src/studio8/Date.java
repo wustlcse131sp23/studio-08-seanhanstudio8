f85e5715427a31a6da862bc76c780cb84fa89eb9
RealSeanHan <h.xuan1@wustl.edu>
2023-04-06 14:16:55 -0500

    1

--- src/studio8/Date.java
+++ src/studio8/Date.java
@@ -1,10 +1,68 @@
 package studio8;
 
+import java.util.Objects;
+
 public class Date {
+	
+	private String Month;
+	private int Date;
+	private int Year;
+	private boolean IfHoliday;
+/**
+ * Constructor for creating a Course object.
+ * @param Month  we choose
+ * @param Date we choose
+ * @param Year we choose
+ */
+	public Date(String Month, int Date, int Year, boolean IfHoliday) {
+		this.Month = Month;
+		this.Date = Date;
+		this.Year = Year;
+		this.IfHoliday = IfHoliday;
+
+	}
+
+	
+	public String GetMonth() {
+		return Month;
+	}
+	
+	public int GetDate() {
+		return Date;
+	}
+	
+	public int GetYear() {
+		return Year;
+	}
 
 
+	public String toString() {
+		return Month +" "+ Date +  " " + Year + " is a holiday is "+ IfHoliday;
+	}
+	
     public static void main(String[] args) {
-
+    	Date date1 = new Date("Jan", 1 , 2023, true);
+    	System.out.println(date1);
+    	
     }
+
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(Date, Month, Year);
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Date other = (Date) obj;
+		return Date == other.Date && Objects.equals(Month, other.Month) && Year == other.Year;
+	}
 
 }
--- src/studio8/Time.java
+++ src/studio8/Time.java
@@ -3,9 +3,41 @@ package studio8;
 
 public class Time {
 
+private int Hour;
+private int Minute;
+private boolean IfHalf;
+
+
 	public static void main(String[] args) {
- 
-    	
+		Time time1 = new Time(12,40, false);
+    	System.out.print(time1);
     }
+	
+	public Time(int Hour, int Minute, boolean IfHalf) {
+		this.Hour = Hour;
+		this.Minute = Minute;
+		this.IfHalf = IfHalf;
+	}
+	
+	public int GetHour() {
+		return Hour;
+	}
+	public int GetMinute() {
+		return Minute;
+	}
+	
+	public String toString() {
+		if (Hour >12 && IfHalf == false) {
+			return "The time is " + (Hour - 12) +  ":" + Minute + "PM";
+		}
+		else if(Hour < 12 && IfHalf == false) {
+			return "The time is " + Hour + ":" + "AM";
+		}
+		else {
+			return "The time is "+ Hour +":" + Minute + "AM";
+		}
+		
+	}
+
 
 }
